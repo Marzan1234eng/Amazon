a89e9afbec93ee47a5410140078fe2f65020f755
Marzan1234eng <mdmarzanhossain@gmail.com>
2021-08-24 14:36:07 +0600

    Product & Order Summery component added

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const {cart} = props;
+    // const total = cart.reduce((total, cart) => total + cart.price , 0);
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+    }
+    let shippingCost = 0;
+    if(total > 35){
+        shippingCost = 0;
+    }else if (total > 15){
+        shippingCost = 4.99;
+    }
+    else if (total > 0){
+        shippingCost = 12.99
+    }
+
+    const tax = Math.round(total / 10).toPrecision(2);
+    const grandTotal = total + shippingCost + Number(tax);
+    return (
+        <div>
+            <h4>Order Summery</h4>
+            <p>Items ordered: {cart.length}</p>
+            <p>Product Price: {total}</p>
+            <p><small>Shipping Cost: {shippingCost}</small></p>
+            <p><small>Vat + Tax: {tax}</small></p>
+            <p>Total Price: {grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import fakeData from "../../fakeData";
+import "./shop.css";
+import Product from "../Product/Product";
+import Cart from "../Cart/Cart";
+
+const Shop = () => {
+
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCard] = useState([]);
+
+    const handleAddProduct = (products) => {
+        console.log("Product Added", products)
+        const newCart = [...cart,products];
+        setCard(newCart);
+    };
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    products.map(
+                        (product) =>
+                            <Product
+                                handleAddProduct = {handleAddProduct}
+                                product={product}>
+                            </Product>
+                    )
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
