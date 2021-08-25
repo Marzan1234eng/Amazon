1b071f6492aaf17a1b77d1391a58f23e3857d9f8
Marzan1234eng <mdmarzanhossain@gmail.com>
2021-08-25 11:28:02 +0600

    props destructure

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -3,9 +3,10 @@ import './product.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-const Product = (props) => {
+const Product = ({product, handleAddProduct}) => {
     /*console.log(props);*/
-    const {img, name , seller, price, stock} = props.product;
+    /*const {product, handleAddProduct} = props*/
+    const {img, name , seller, price, stock} = product;
 
 
     return (
@@ -20,7 +21,7 @@ const Product = (props) => {
                 <p><small>Only {stock} left in stock - order soon</small></p>
                 <button
                     className="main-btn"
-                    onClick={() => props.handleAddProduct(props.product)}
+                    onClick={() => handleAddProduct(product)}
                 >
                     <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
             </div>
